6b1f8fb19879155835cdef16e459aa25e75fb1f8
michpil19 <pilchu2501@gmail.com>
2022-09-18 12:11:03 +0200

    Initial commit

---
+++ SocialForceModel/src/main/java/DesktopLauncher.java
@@ -0,0 +1,14 @@
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+    public static void main(String[] args) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setIdleFPS(60);
+        config.useVsync(true);
+        config.setTitle("SFM");
+
+        config.setWindowedMode(1280,720);
+        new Lwjgl3Application(new Boot(), config);
+    }
+}
